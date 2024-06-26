c6fc2973cc4b792b26d80bf8ba2513f4b3494431
FarrizFikri <farrizfikri@gmail.com>
2024-06-26 12:07:47 +0800

    scope variable

---
+++ Python/scopeofvariable.py
@@ -0,0 +1,56 @@
+def add(a,b):
+    #result is a local variable
+    #variable created inside function
+    # this variable cannot be accessed from outside the function
+    result = a + b
+    return result
+
+#print (result)
+
+#The above discussion is called scope of a variable
+#Now we are at the main context is also called global context
+x = 10
+#variable x is created at the main context/ global context
+def printX():
+    #can i access the variable x created at the main context
+    #inside the function context (printX)
+    print("Inside the function printX:",x)
+
+printX()
+
+def modifyX():
+    #we are trying to modify value of variable x
+    #whenever you try to modify a variable the variable is
+    #immediately created at the local context
+    #it does not refer to the global context
+    x = 15
+    print("Inside the function (modifyX):",x)
+
+modifyX()
+print(x)
+
+#If you really want a function to modify the variable in the 
+#global context then the function must return the value
+#which again must be assigned to global variable
+
+def traditionalModifyX():
+    x = 15
+    return x
+
+x = traditionalModifyX()
+print(x)
+#proper programming recommendation
+
+#however in python we also have a short cut
+#because if follow traditional method, function always return single value
+#but what if we want to modify multiple variables?
+
+def pythonModifyX():
+    global x #telling python dont create x locally
+    x = 25
+    print("Inside the function (pythonModifyX):",x)
+
+pythonModifyX()
+print(x)
+
+
