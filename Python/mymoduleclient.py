208fd9b6b8777dbfdc5a581cd262fd88ef3fab2d
FarrizFikri <farrizfikri@gmail.com>
2024-06-28 10:33:47 +0800

    print

--- Python/mymoduleclient.py
+++ Python/mymoduleclient.py
@@ -4,8 +4,11 @@ print("Interest:", mymodule.simpleInterest(1000,1,6))
 
 #Second method
 #better because dont need to type module name everytime
+#python recommend this method
 from mymodule import simpleInterest
 print("Interest:", simpleInterest(1000,1,6))
 
-
+#Third method 
+from mymodule import *
+print("Interest:", simpleInterest(1000,1,6))
 
