62535d1a22a596642ab5875a0f147f4b54a327c1
FarrizFikri <farrizfikri@gmail.com>
2024-07-03 10:22:23 +0800

    "Updated ebox.py to add a Demo class and modified errors.py to fix syntax and logical errors, and added try-except block for runtime error handling."

--- Python/ebox.py
+++ Python/ebox.py
@@ -1,4 +1,10 @@
-def C2F(c):
-    return(c * 9//5 + 32)
-print(C2F(100)),
-print(C2F(0))
+
+class Demo:
+    def __init__(self):
+        self.a = 1
+        self.__b = 1
+    def get(self):
+        return self.__b
+obj = Demo()
+obj.a=45
+print(obj.a)
--- Python/errors.py
+++ Python/errors.py
@@ -1,19 +1,31 @@
-x = 10
+x = 11
 
-#Syntax error
-# if (x % 2 ==0):
-# print("x is even")
-#IndentationError: expected an indented block after 'if' statement on line 2
+# Syntax Error
+# if (x % 2 == 0):
+# print("Even Number")
 
-#Logical Error
-# if (x % 2 ==0):
-#       print(f"Given Number is {x}")
-# print("x is even")
+# Logical Error
+# if (x % 2 == 0):
+#    print(f"Given Number is {x}")
+# print("Even Number")
 
-#Runtime Error
-principle = int(input ("Principle: "))
-period = int(input ("Period:"))
-rate = int(input ("Rate:"))
-interest = (principle * period * rate )/100
-print ("Interest Amount:", interest)
+# Runtime Error
+
+try:
+    # we know the following line is taking user input
+    # in future this may throw error
+    # then you must place this code inside a block called
+    # try except
+    principle = int(input("Principle: "))
+except:
+    # when that error occur what we must do
+    print("Principle amount must be an Integer")
+    principle = 1000
+
+# The program does not get terminated abnormally
+
+period = int(input("Period: "))
+rate = int(input("Rate: "))
+interest = (principle * period * rate) / 100
+print("Interest Amount: ", interest)
 
